92e9b93a1ddd68db907dbb4fd1d92d598d6cb348
Klaus Kämpf <kkaempf@suse.de>
2018-03-08 14:18:14 +0100

    do not hide docbookrx errors

--- adoc.sh
+++ adoc.sh
@@ -3,7 +3,7 @@ export FILE=$1
 export NAME=`basename $1 .xml`
 echo "Converting $NAME"
 rm -rf asciidoctor
-docbookrx xml/$NAME.xml 2>out.err
+docbookrx xml/$NAME.xml
 asciidoctor -b docbook5 -d book -D asciidoctor/xml xml/$NAME.adoc
 daps -m asciidoctor/xml/$NAME.xml --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
 cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/MAIN-manager/html/MAIN-manager/images
