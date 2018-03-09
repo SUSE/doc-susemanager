907db50f3c74df8d100e6aa98bb0860eaa5bed2c
Klaus Kämpf <kkaempf@suse.de>
2018-03-09 11:46:50 +0100

    enable docbookrx strict parsing to enable entities

--- adoc.sh
+++ adoc.sh
@@ -3,7 +3,8 @@ export FILE=$1
 export NAME=`basename $1 .xml`
 echo "Converting $NAME"
 rm -rf asciidoctor
-docbookrx xml/$NAME.xml
+docbookrx --strict xml/$NAME.xml
+#asciidoctor -b html -d book -D asciidoctor/html xml/$NAME.adoc
 asciidoctor -b docbook5 -d book -D asciidoctor/xml xml/$NAME.adoc
 daps -m asciidoctor/xml/$NAME.xml --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
 cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/MAIN-manager/html/MAIN-manager/images
