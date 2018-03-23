a9f5b9afaa0d1aa06bc52c6c119961a09d099e91
Klaus Kämpf <kkaempf@suse.de>
2018-03-23 12:12:49 +0100

    Initial xml->adoc conversion

--- adoc.sh
+++ adoc.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-export FILE=$1
-export NAME=`basename $1 .xml`
+export FILE=xml/MAIN-manager.xml
+export NAME=`basename $FILE .xml`
 echo "Converting $NAME"
 rm -rf asciidoctor
 docbookrx --strict xml/$NAME.xml
+mv xml/*adoc adoc
 #asciidoctor -b html -d book -D asciidoctor/html xml/$NAME.adoc
-asciidoctor -b docbook5 -d book -D asciidoctor/xml xml/$NAME.adoc
+asciidoctor -b docbook5 -d book -D asciidoctor/xml adoc/$NAME.adoc
 daps -m asciidoctor/xml/$NAME.xml --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
 cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/MAIN-manager/html/MAIN-manager/images
