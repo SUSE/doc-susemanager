06b186b64f40b79aa8b8379a70661adb8e044137
Klaus Kämpf <kkaempf@suse.de>
2018-04-04 18:18:58 +0200

    update adoc

--- adoc.sh
+++ adoc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export FILE=xml/MAIN-manager.xml
+export FILE=$1 #xml/MAIN-manager.xml
 export NAME=`basename $FILE .xml`
 echo "Converting $NAME"
 rm -rf asciidoctor
@@ -8,4 +8,4 @@ mv xml/*adoc adoc
 #asciidoctor -b html -d book -D asciidoctor/html xml/$NAME.adoc
 asciidoctor -b docbook5 -d book -D asciidoctor/xml adoc/$NAME.adoc
 daps -m asciidoctor/xml/$NAME.xml --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
-cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/MAIN-manager/html/MAIN-manager/images
+cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/$NAME/html/$NAME/images
