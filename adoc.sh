f180afe7a1b294ef90a0badb13611acd290a74f2
Klaus Kämpf <kkaempf@suse.de>
2018-04-10 11:10:09 +0200

    adoc.sh: enable xml entities

--- adoc.sh
+++ adoc.sh
@@ -7,5 +7,11 @@ docbookrx --strict xml/$NAME.xml
 mv xml/*adoc adoc
 #asciidoctor -b html -d book -D asciidoctor/html xml/$NAME.adoc
 asciidoctor -b docbook5 -d book -D asciidoctor/xml adoc/$NAME.adoc
+# insert ENTITY
+sed -i '2i <!DOCTYPE set [ <!ENTITY % entities SYSTEM "entity-decl.ent"> %entities; ]>' asciidoctor/$1
+# replace {foo} (but not ${foo}) with &foo;
+perl -p -i -e 's/([^\$])\{(\w+)\}/\1\&$2\;/g' asciidoctor/$1
+# make .ent files available
+cp xml/*ent asciidoctor/xml
 daps -m asciidoctor/xml/$NAME.xml --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
 cp images/src/png/*.png images/src/svg/*.svg asciidoctor/build/$NAME/html/$NAME/images
