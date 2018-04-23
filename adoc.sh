90a3539113843a3a7346dfe555439a6e2e803bdd
Klaus Kämpf <kkaempf@suse.de>
2018-04-23 15:21:30 +0200

    XML to ASCIIDOCTOR
    - remove xml/*.xml
    - replace with adoc/*.adoc
    - keep xml/*.ent
    - move images/*/* to adoc/images
    - adapt adoc.sh script
    - .gitignore asciidoctor/
    
    Run 'adoc.sh' to generate .html with daps.
    Build results will be in asciidoctor/build/...

--- adoc.sh
+++ adoc.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-export FILE=$1 #xml/MAIN-manager.xml
+export FILE=xml/MAIN-manager.xml
 export NAME=`basename $FILE .xml`
-echo "Converting $NAME"
-rm -rf asciidoctor
-docbookrx --strict xml/$NAME.xml
-mkdir -p adoc
-mv xml/*adoc adoc
+#echo "Converting $NAME"
+#rm -rf asciidoctor
+#docbookrx --strict xml/$NAME.xml
+#mkdir -p adoc
+#mv xml/*adoc adoc
 #asciidoctor -b html -d book -D asciidoctor/html xml/$NAME.adoc
 asciidoctor -b docbook5 -d book -D asciidoctor/xml adoc/$NAME.adoc
 # insert ENTITY
-sed -i '2i <!DOCTYPE set [ <!ENTITY % entities SYSTEM "entity-decl.ent"> %entities; ]>' asciidoctor/$1
+sed -i '2i <!DOCTYPE set [ <!ENTITY % entities SYSTEM "entity-decl.ent"> %entities; ]>' asciidoctor/$FILE
 # replace {foo} (but not ${foo}) with &foo;
-perl -p -i -e 's/([^\$])\{(\w+)\}/\1\&$2\;/g' asciidoctor/$1
+perl -p -i -e 's/([^\$])\{(\w+)\}/\1\&$2\;/g' asciidoctor/$FILE
 # make .ent files available
 cp xml/*ent asciidoctor/xml
 daps -m asciidoctor/xml/$NAME.xml --verbosity=0 --styleroot /usr/share/xml/docbook/stylesheet/suse2013-ns html
