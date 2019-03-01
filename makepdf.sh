b83d387ed2ea5380c50a3b600ea97e7c38a48a5f
Joseph Cayouette <jcayouette@suse.com>
2019-03-01 09:05:46 +0100

    added pdf templates, the  pdf-constructor for building pdf books, and moved search ui and antora.yml to base repo. we do not need a publisher repo

--- makepdf.sh
+++ makepdf.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-asciidoctor-pdf \
+docker run -it -v  /home/coyote/workspace/doc-susemanager-publisher/workspace/doc-susemanager/pdf_template/installation-guide-pdf:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf book_mgr_installation.adoc \
 -a toc-title=Contents \
--a pdf-style=suse \
--a pdf-stylesdir=$SUSEDOC/pdf_template \
+-a chapter-label= \
+-a pdf-style=draft \
+-a pdf-stylesdir=./ \
 -a icons=font \
--a pdf-fontsdir=$SUSEDOC/pdf_template/fonts \
+-a pdf-fontsdir=fonts \
 -a source-highlighter=coderay \
 $1
