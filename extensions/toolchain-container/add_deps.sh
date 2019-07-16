7d901a823ecd57cca9bc79006377730552221527
Joseph Cayouette <jcayouette@suse.com>
2019-07-16 13:18:07 +0200

    update

--- extensions/toolchain-container/add_deps.sh
+++ extensions/toolchain-container/add_deps.sh
@@ -4,6 +4,7 @@ set -e
 
 # Install rbenv dependencies
 zypper --gpg-auto-import-keys ref
-zypper --non-interactive install git tar automake gdbm-devel libyaml-devel unzip sqlite3-devel ncurses-devel readline-devel zlib-devel git gcc gzip libopenssl-devel wget curl bzip make ruby2.5 ruby2.5-devel vim
+zypper --non-interactive install git tar nodejs10 nodejs10-devel npm automake gdbm-devel libyaml-devel unzip sqlite3-devel ncurses-devel readline-devel zlib-devel git gcc gzip libopenssl-devel wget curl bzip make ruby2.5 ruby2.5-devel vim
 zypper --non-interactive clean --all
+
 
